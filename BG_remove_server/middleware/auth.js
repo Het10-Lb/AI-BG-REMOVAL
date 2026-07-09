8eb1bcee56ac90da9709298a673d354d86a024e7
Het10-Lb <23cshet094@ldce.ac.in>
2026-07-09 11:38:37 +0530

    cors added

--- BG_remove_server/middleware/auth.js
+++ BG_remove_server/middleware/auth.js
@@ -10,7 +10,7 @@ const authUser = async (req,res, next)=>{
        }
 
        const token_decode = jwt.decode(token)
-       console.log(token_decode);
+    //    console.log(token_decode);
        
        req.clerkId = token_decode.clerkId
        next()
--- BG_remove_server/server.js
+++ BG_remove_server/server.js
@@ -9,9 +9,13 @@ import imageRouter from './routes/imageRouter.js';
 //APP CONFIG
 const PORT = process.env.PORT || 3000;
 const app = express();
-app.use(cors());
+app.use(cors(
+  {
+    origin: process.env.FRONTEND_URL
+  }
+));
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));  
 
 
 
